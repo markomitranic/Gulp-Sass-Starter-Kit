07eb792567eafbecbb7b12a788baf35693ccc3f8
Your Name <you@example.com>
2018-04-04 11:34:32 +0200

    Add eslint config

--- scripts/domain/cookie-test.js
+++ scripts/domain/cookie-test.js
@@ -1,6 +1,13 @@
 agCookie.create('example-cookie', true, 1);
 
 var readValue = agCookie.read('example-cookie');
+
 console.log(readValue);
 
 agCookie.erase('example-cookie');
+
+function markoFunkcija(args) {
+    console.log(args);
+    
+    return true;
+}
